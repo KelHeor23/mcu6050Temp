7935d9e9484a70d850e8ad9a9b33d720ae57fb23
Alex <kelheor23@gmail.com>
2025-10-24 02:16:12 +0300

    fix print

--- main.c
+++ main.c
@@ -4,24 +4,20 @@
 
 int main(void)
 {
-    // Замените путь, если у вас i2c-1
-    mpu6050_t *m = mpu6050_init("/dev/i2c-0");
+    mpu6050_t *m = mpu6050_init("/dev/i2c-2");
     if (!m) {
         fprintf(stderr, "MPU6050 init failed\n");
         return 1;
     }
-
-    // Пример: убедимся, что чип «проснулся»
-    // (mpu6050_init уже делает write в PWR_MGMT_1 = 0x00)
-    // При желании можно выставить DLPF:
-    // mpu6050_set_config(m, 0x03);
 
     printf("ax,ay,az,gx,gy,gz\n");
     while (1) {
         int16_t ax, ay, az, gx, gy, gz;
         mpu6050_get_accl_raw(m, &ax, &ay, &az);
         mpu6050_get_gyro_raw(m, &gx, &gy, &gz);
-        printf("%d,%d,%d,%d,%d,%d\n", ax, ay, az, gx, gy, gz);
+        printf("ax = %.2f ay = %.2f, az = %.2f gx = %.2f gy = %.2f gz = %.2f\n",
+            (float)ax / 16384, (float)ay / 16384, (float)az / 16384,
+            (float)gx / 131, (float)gy / 131, (float)gz / 131);
         fflush(stdout);
         usleep(100000); // 100 ms
     }
